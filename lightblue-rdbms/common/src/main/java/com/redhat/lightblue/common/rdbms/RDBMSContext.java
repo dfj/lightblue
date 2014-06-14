cd478efed99749153e6da774fcc5108e8bd7ca18
Luan Cestari <luan.cestari@gmail.com>
2014-06-13 23:36:01 -0300

    Changes for the intergration between lightblue and RDBMS

--- lightblue-rdbms/common/src/main/java/com/redhat/lightblue/common/rdbms/RDBMSContext.java
+++ lightblue-rdbms/common/src/main/java/com/redhat/lightblue/common/rdbms/RDBMSContext.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class RDBMSContext <T> {
 
@@ -18,7 +19,8 @@ public class RDBMSContext <T> {
     private ResultSet ResultSet = null;
     private RowMapper<T> rowMapper = null;
     private List<T> resultList = null;
-    private HashMap<Object,Object> parameters = null;
+    private Set<Parameter> parameters = null;
+    private Set<TableField> tableFields = null;
 
     public DataSource getDataSource() {
         return dataSource;
@@ -92,51 +94,20 @@ public class RDBMSContext <T> {
         this.resultList = resultList;
     }
 
-    public HashMap<Object, Object> getParameters() {
+    public Set<Parameter> getParameters() {
         return parameters;
     }
 
-    public void setParameters(HashMap<Object, Object> parameters) {
+    public void setParameters(Set<Parameter> parameters) {
         this.parameters = parameters;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        RDBMSContext that = (RDBMSContext) o;
-
-        if (ResultSet != null ? !ResultSet.equals(that.ResultSet) : that.ResultSet != null) return false;
-        if (connection != null ? !connection.equals(that.connection) : that.connection != null) return false;
-        if (dataSource != null ? !dataSource.equals(that.dataSource) : that.dataSource != null) return false;
-        if (dataSourceName != null ? !dataSourceName.equals(that.dataSourceName) : that.dataSourceName != null)
-            return false;
-        if (parameters != null ? !parameters.equals(that.parameters) : that.parameters != null) return false;
-        if (preparedStatement != null ? !preparedStatement.equals(that.preparedStatement) : that.preparedStatement != null)
-            return false;
-        if (resultInteger != null ? !resultInteger.equals(that.resultInteger) : that.resultInteger != null)
-            return false;
-        if (resultList != null ? !resultList.equals(that.resultList) : that.resultList != null) return false;
-        if (rowMapper != null ? !rowMapper.equals(that.rowMapper) : that.rowMapper != null) return false;
-        if (statement != null ? !statement.equals(that.statement) : that.statement != null) return false;
-
-        return true;
+    public Set<TableField> getTableFields() {
+        return tableFields;
     }
 
-    @Override
-    public int hashCode() {
-        int result = dataSource != null ? dataSource.hashCode() : 0;
-        result = 31 * result + (dataSourceName != null ? dataSourceName.hashCode() : 0);
-        result = 31 * result + (connection != null ? connection.hashCode() : 0);
-        result = 31 * result + (statement != null ? statement.hashCode() : 0);
-        result = 31 * result + (preparedStatement != null ? preparedStatement.hashCode() : 0);
-        result = 31 * result + (resultInteger != null ? resultInteger.hashCode() : 0);
-        result = 31 * result + (ResultSet != null ? ResultSet.hashCode() : 0);
-        result = 31 * result + (rowMapper != null ? rowMapper.hashCode() : 0);
-        result = 31 * result + (resultList != null ? resultList.hashCode() : 0);
-        result = 31 * result + (parameters != null ? parameters.hashCode() : 0);
-        return result;
+    public void setTableFields(Set<TableField> tableFields) {
+        this.tableFields = tableFields;
     }
 
     @Override
@@ -152,6 +123,7 @@ public class RDBMSContext <T> {
                 ", rowMapper=" + rowMapper +
                 ", resultList=" + resultList +
                 ", parameters=" + parameters +
+                ", tableFields=" + tableFields +
                 '}';
     }
 }
